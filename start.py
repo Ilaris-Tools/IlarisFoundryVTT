b5cc46bd774b801fba24878c32ee1eb8c1791f24
feorg <6203678-Feorg@users.noreply.gitlab.com>
2021-03-14 15:38:16 +0100

    personal settings file

--- start.py
+++ start.py
@@ -1,11 +1,20 @@
 import shutil
 import subprocess
+import settings_local
 
-folder_dst = '/home/feorg/Documents/FoundryVTT/Data/systems/Ilaris'
-folder_src = '/home/feorg/Documents/Programming/foundryvtt/systems - working javascript/Ilaris-FoundryVTT'
-folder_vtt = '/home/feorg/Downloads/foundry_vtt/foundryvtt-0.7.9/foundryvtt'
-start_world = '--world=test_ilaris'
+"""
+Beispiel Settings in settings_local.py:
+folder_destination = '/home/feorg/Documents/FoundryVTT/Data/systems/Ilaris'
+folder_source = '/home/feorg/Documents/Programming/foundryvtt/systems - working javascript/Ilaris-FoundryVTT'
+folder_Foundryvtt = '/home/feorg/Downloads/foundry_vtt/foundryvtt-0.7.9/foundryvtt'
+start_world_options = '--world=test_ilaris'
+"""
 
-shutil.rmtree(folder_dst)
-shutil.copytree(src=folder_src, dst=folder_dst)
-subprocess.call([folder_vtt, start_world])
+folder_destination = settings_local.folder_destination
+folder_source = settings_local.folder_source
+folder_Foundryvtt = settings_local.folder_Foundryvtt
+start_world_options = settings_local.start_world_options
+
+shutil.rmtree(folder_destination)
+shutil.copytree(src=folder_source, dst=folder_destination)
+subprocess.call([folder_Foundryvtt, start_world_options])
