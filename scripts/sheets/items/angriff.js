9a5b10461f458ecd35202cdc1dfc000bf0e0a815
Lukas R <mail@sarbot.de>
2022-09-23 22:45:56 +0200

    waffeneigenschaften in angriffen

--- scripts/sheets/items/angriff.js
+++ scripts/sheets/items/angriff.js
@@ -6,4 +6,54 @@ export class AngriffSheet extends IlarisItemSheet {
             template: 'systems/Ilaris/templates/sheets/items/angriff.html',
         });
     }
+    
+    activateListeners(html) {
+        super.activateListeners(html);
+        console.log("Angriff listeners");
+        html.find('.add-eigenschaft').click((ev) => this._onAddEigenschaft(ev));
+        html.find('.del-eigenschaft').click((ev) => this._onDelEigenschaft(ev));
+    }
+
+    _getSubmitData(updateData={}) {
+        const data = super._getSubmitData(updateData);
+        // dirty workaround for v9 to turn objects back into arrays. 
+        // In V10 a datamodel with schema should solve this, by directly using arrays
+        console.log("data");
+        // console.log(data);
+        let eigenschaften = [];
+        for (const [key, value] of Object.entries(data)) {
+            if (key.startsWith("data.eigenschaften") && key.endsWith("name")) {
+                var idx = key.split(".")[2];
+                eigenschaften.push({name: value, text: data[`data.eigenschaften.${idx}.text`]})
+                delete data[key];
+                delete data[`data.eigenschaften.${idx}.text`]
+            }
+        }
+        console.log(eigenschaften);
+        data["data.eigenschaften"] = eigenschaften;
+        return data;
+    }
+
+    // _updateObject(event, formData){
+    //     console.log("formdata");
+    //     console.log(formData);
+    //     formData.data.eigenschaften = [];
+    //     return super._updateObject(event, formData);
+    // }
+    
+    _onAddEigenschaft(event) {
+        //let item = this.document.data;
+        console.log(this.document);
+        this.document.data.data.eigenschaften.push({name: "Neue Eigenschaft", text: ""});
+        console.log(this.document);
+        this.document.render();
+    }
+
+    _onDelEigenschaft(event){
+        let eigid = $(event.currentTarget).data('eigenschaftid');
+        //console.log(`remove: ${eigid}`);
+        this.document.data.data.eigenschaften.splice(eigid, 1);
+        this.document.render();
+    }
+
 }
