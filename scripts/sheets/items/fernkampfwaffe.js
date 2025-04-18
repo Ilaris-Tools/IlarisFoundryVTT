fb2a0e801bbaec4f06e5ac276d84fca544c95bca
patq <padiquell@gmx.de>
2025-04-18 14:44:51 +0200

    27: adds pattern check for tp

--- scripts/sheets/items/fernkampfwaffe.js
+++ scripts/sheets/items/fernkampfwaffe.js
@@ -10,6 +10,7 @@ export class FernkampfwaffeSheet extends IlarisItemSheet {
     }
 
     static get defaultOptions() {
+        console.log("defaultOptions",this);
         return foundry.utils.mergeObject(super.defaultOptions, {
             // classes: ["ilaris", "sheet"],
             template: 'systems/Ilaris/templates/sheets/items/fernkampfwaffe.html',
