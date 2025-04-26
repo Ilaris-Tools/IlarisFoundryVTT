101d3f4a9be632c9cd27e5ac984eef6c1a62c693
pattt333 <padiquell@gmx.de>
2025-04-26 21:29:51 +0200

    addManoeverDynamically: (#15)
    * addManoeverDynamically:
    - adds type selection for kreaturen angriff
    - renames angriffitem to combatitem
    - waffen use combatitem as base
    - adds maneuver from compendium pack
    - adds ilaris system config for maneuver pack
    
    * addManoeverDynamically:
    - adds damage migration for weapons
    
    * update links, clean up readme and contribution
    
    * GH workflow, release strategy
    
    * refer to README
    
    * PR Templates
    
    * Kompatibilitätshinweise foundry versionen
    
    * mkdocs config
    
    * add some first text to landing page
    
    * sephrasto export und erste fragen formuliert
    
    ---------
    
    Co-authored-by: Lukas <mail@sarbot.de>

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -233,6 +233,19 @@ Hooks.once('init', () => {
             icon: 'systems/Ilaris/assets/images/icon/swordwoman-orange.svg',
         },
     ];
+
+    game.settings.register('Ilaris', 'IlarisManoeverPaket', {
+        name: 'Ilaris Manoever Paket',
+        hint: 'Bestimmt welche Paket aus dem Compendium verwendet werden soll. Per Default werden die Manöver von Ilaris verwendet (Ilaris.manoever (System/Modulname.Manöverpaketname)). Diese Einstellung sollte am Besten nur beim nach Erstellung einer neuen Welt geändert werden, danach wird es dazu führen, dass es Helden/Kreaturen mit Manövern aus anderen Kompendien gibt.',
+        scope: 'world',
+        config: true,
+        type: String,
+        default: 'Ilaris.manover',
+        onChange: value => { // value is the new value of the setting
+          console.log(value)
+        },
+        requiresReload: true, // true if you want to prompt the user to reload
+      });
 });
 
 Hooks.on('applyActiveEffect', (actor, data, options, userId) => {
---
+++ scripts/items/combat.js
@@ -0,0 +1,25 @@
+import { IlarisItem } from "./item.js";
+
+export class CombatItem extends IlarisItem {
+    async setManoevers() {
+        // TODO: könnte man vlt. dynamisch machen:
+        //manoever davor mehr typen geben melee, ranged, magic, liturgie, commandos, intimidate
+        //einstellungs option für system geben, um hausregel comp pack zu verwenden statt standard ilaris
+        // alle aus dem pack und dann vorteile prüfen
+        const manoeverPack = game.packs.get(game.settings.get('Ilaris', 'IlarisManoeverPaket'));
+        const manoeverItemData = (await manoeverPack.getDocuments());
+        if("nahkampfwaffe" === this.type || ("angriff" === this.type && this.system.typ === "Nah")) {
+            this.manoever = [];
+            manoeverItemData.forEach(manoever => {
+                if(manoever.system.gruppe === 0) {
+                    this.manoever.push(manoever);
+                }
+            });
+        }
+        if("fernkampfwaffe" === this.type  || ("angriff" === this.type && this.system.typ === "Fern")) {
+        }
+        // this.system.manoever.kbak = {selected: false}
+        // TODO: deepcopy from config hier rein und filtern 
+        // oder possible flag setzen je nach vorteilen des owners.
+    }
+}
