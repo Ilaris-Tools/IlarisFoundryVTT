11947461c2270ddd0d2ea5a8200b356e983e0c9c
Siebenstreich <padiquell@gmx.de>
2026-01-19 16:35:36 +0100

    Fix/fixes 6 (#358)
    * fixes test no error throwing for ifGt
    
    * fixes rounding

--- scripts/common/_spec/handlebars.spec.js
+++ scripts/common/_spec/handlebars.spec.js
@@ -15,20 +15,6 @@ describe('handlebars', () => {
     })
 
     describe('ifGt', () => {
-        describe('error', () => {
-            test.each`
-                description            | val1              | val2
-                ${'val1 !number'}      | ${'invalid Val1'} | ${2}
-                ${'val1 == null'}      | ${null}           | ${2}
-                ${'val2 !number'}      | ${123}            | ${'string val2'}
-                ${'val2 == undefined'} | ${123}            | ${undefined}
-            `('$description', ({ val1, val2 }) => {
-                expect(() => {
-                    helpers.ifGt(val1, val2)
-                }).toThrow('handelbars.js - ifGt - atleast one parameter is not a number')
-            })
-        })
-
         describe('all numbers', () => {
             test.each`
                 description       | val1  | val2  | resultValue
