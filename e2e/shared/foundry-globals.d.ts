0d50a86c8abcb5ea55d0a4bc5c31054bcedf01e5
patq <padiquell@gmx.de>
2026-04-19 14:41:25 +0200

    e2e test to check crits and crit fails

--- e2e/shared/foundry-globals.d.ts
+++ e2e/shared/foundry-globals.d.ts
@@ -6,4 +6,12 @@ declare global {
     const ChatMessage: {
         deleteDocuments(ids: string[]): Promise<unknown>
     }
+
+    const CONFIG: {
+        Dice: {
+            randomUniform?: () => number
+            [key: string]: unknown
+        }
+        [key: string]: unknown
+    }
 }
